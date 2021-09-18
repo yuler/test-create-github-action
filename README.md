a29ece95ce06eaf5e53002e5b3d35f4d761c0c7c
yuler <joyful4yu@gmail.com>
2021-09-18 12:05:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Test Create GitHub Action
+
+> This repo is for test create github action
+
+## Refs:
+
+-   https://docs.github.com/en/actions/creating-actions
---
+++ index.js
@@ -0,0 +1,15 @@
+import {getInput, setOutput, setFailed} from '@actions/core'
+import {context} from '@actions/github'
+
+try {
+	// `who-to-greet` input defined in action metadata file
+	const nameToGreet = getInput('who-to-greet')
+	console.log(`Hello ${nameToGreet}!`)
+	const time = new Date().toTimeString()
+	setOutput('time', time)
+	// Get the JSON webhook payload for the event that triggered the workflow
+	const payload = JSON.stringify(context.payload, undefined, 2)
+	console.log(`The event payload: ${payload}`)
+} catch (error) {
+	setFailed(error.message)
+}
