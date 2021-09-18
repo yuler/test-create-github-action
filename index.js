9bb0c6a1c7cdb91e694289f4a8d58a74c893bdb5
yuler <joyful4yu@gmail.com>
2021-09-18 12:58:18 +0800

    feat: console log add `Local` prefix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {context} from '@actions/github'
 try {
 	// `who-to-greet` input defined in action metadata file
 	const nameToGreet = getInput('who-to-greet')
-	console.log(`Hello ${nameToGreet}!`)
+	console.log(`Local Hello ${nameToGreet}!`)
 	const time = new Date().toTimeString()
 	setOutput('time', time)
 	// Get the JSON webhook payload for the event that triggered the workflow
